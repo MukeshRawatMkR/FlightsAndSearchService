37173b4146ef5b449e0ab5e8c7c24be9b0012eab
MukeshRawatMkR <swissmukeshrawat@gmail.com>
2024-05-10 19:44:46 +0530

    setup basic dotenv

---
+++ src/config/serverConfig.js
@@ -0,0 +1,8 @@
+const dotenv=require('dotenv');
+
+dotenv.config();
+
+module.exports={
+    PORT:process.env.PORT
+}
+require('dotenv').config()// to display port no on console otherwise it doesn't display onto console.
